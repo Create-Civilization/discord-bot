05b0c8bab1113687937f6ee804b5ceb745e8e74c
Arctic <creepergaming490@gmail.com>
2025-03-24 22:57:02 -0400

    e

---
+++ commands/server_commands/force_update_whitelist.js
@@ -0,0 +1,25 @@
+const { getMinecraftNameByUUID } = require('../../other_functions/helperFunctions.js');
+const { getAllWhitelistData } = require('../../other_functions/whitelistDatabaseFuncs.js');
+const { checkForIgnChanges } = require('../../other_functions/tasks.js')
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('force_update_whitelist')
+        .setDescription('force_update_whitelist'),
+    async execute(client, interaction) {
+        if(interaction.user.id !== "336891653291900949") {
+            return interaction.reply({
+                content: 'You do not have permission to run this command',
+            });
+        }
+
+        await interaction.deferReply({ ephemeral: true });
+        try {
+           await checkForIgnChanges(client);
+        } catch (err) {
+            console.log(err);
+            await interaction.editReply('An error occurred while updating whitelist.');
+        }
+    }
+};
