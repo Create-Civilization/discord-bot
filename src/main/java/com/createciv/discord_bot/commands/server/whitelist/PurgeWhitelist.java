602054222202fad54da252698acee78a6d947a9c
Arctic <creepergaming490@gmail.com>
2025-05-03 01:26:08 -0400

    Unfinished PurgeWhitelist command.
    Signed-off-by: Arctic <creepergaming490@gmail.com>

---
+++ src/main/java/com/createciv/discord_bot/commands/server/whitelist/PurgeWhitelist.java
@@ -0,0 +1,53 @@
+package com.createciv.discord_bot.commands.server.whitelist;
+
+import com.createciv.discord_bot.Bot;
+import com.createciv.discord_bot.ConfigLoader;
+import com.createciv.discord_bot.classes.SlashCommand;
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+
+public class PurgeWhitelist extends SlashCommand {
+
+    public PurgeWhitelist() {
+        super("purge_whitelist", "purge whitelist of all non members");
+    }
+
+    //TODO Make this command work, should check the database to see if any of the whitelists contain a user no longer in the server
+
+    @Override
+    public void execute(SlashCommandInteractionEvent interactionEvent) {
+        try {
+
+            if (!hasPermission(interactionEvent)) return;
+
+
+        } catch (Exception e) {
+            Bot.LOGGER.error("Error occurred in PurgeWhitelist: {}", e.getMessage(), e);
+        }
+    }
+
+    private boolean hasPermission(SlashCommandInteractionEvent interactionEvent) {
+
+        if (ConfigLoader.ADMIN_ROLE_IDS.isEmpty()) {
+            Bot.LOGGER.error("No admin roles selected. Please set this in the config to continue");
+            interactionEvent.reply("Admin roles are not configured. Please configure them to use this command")
+                    .setEphemeral(true).queue();
+            return false;
+        }
+
+        if (interactionEvent.getMember() == null) {
+            interactionEvent.reply("Could not find member information").setEphemeral(true).queue();
+            return false;
+        }
+
+        boolean hasAdminRole = interactionEvent.getMember().getRoles().stream()
+                .map(Role::getId)
+                .anyMatch(ConfigLoader.ADMIN_ROLE_IDS::contains);
+
+        if (!hasAdminRole) {
+            interactionEvent.reply("You do not have permission to run this command").setEphemeral(true).queue();
+            return false;
+        }
+        return true;
+    }
+}
