9f48f47737177459ead22b45d1d897a43e3ebe0b
McArctic <creepergaming490@gmail.com>
2025-09-02 18:36:02 -0400

    I am bored. So I am done for now.

--- src/main/java/com/createciv/discord_bot/schedualedTasks/tasks/UpdateUsernameCache.java
+++ src/main/java/com/createciv/discord_bot/schedualedTasks/tasks/UpdateUsernameCache.java
@@ -1,7 +1,12 @@
 package com.createciv.discord_bot.schedualedTasks.tasks;
 
 import com.createciv.discord_bot.classes.ScheduledTask;
+import com.createciv.discord_bot.util.database.DatabaseRegistry;
+import com.createciv.discord_bot.util.database.managers.UsernameCacheTable;
+import com.createciv.discord_bot.util.database.types.UsernameCacheEntry;
 
+import java.sql.SQLException;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class UpdateUsernameCache extends ScheduledTask {
@@ -12,6 +17,16 @@ public class UpdateUsernameCache extends ScheduledTask {
 
     @Override
     public void execute() {
-
+        UsernameCacheTable table = (UsernameCacheTable) DatabaseRegistry.getTableManager("usernameCache");
+        try {
+            List<UsernameCacheEntry> cacheEntries = table.getExpired();
+            if (cacheEntries != null) {
+                for (UsernameCacheEntry usernameCacheEntry : cacheEntries) {
+                   // table.remove(usernameCacheEntry.playerUUID);
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
