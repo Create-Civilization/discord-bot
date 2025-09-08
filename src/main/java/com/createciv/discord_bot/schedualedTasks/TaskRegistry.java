a53c8a70f7a6ab3498bf0b90ef7b8fac231ed082
Arctic <creepergaming490@gmail.com>
2025-09-08 13:14:37 -0400

    Its spamming errors at me. I am scared.

--- src/main/java/com/createciv/discord_bot/schedualedTasks/TaskRegistry.java
+++ src/main/java/com/createciv/discord_bot/schedualedTasks/TaskRegistry.java
@@ -2,6 +2,7 @@ package com.createciv.discord_bot.schedualedTasks;
 
 import com.createciv.discord_bot.Bot;
 import com.createciv.discord_bot.classes.ScheduledTask;
+import com.createciv.discord_bot.util.LoggingUtil;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -10,10 +11,17 @@ public class TaskRegistry {
 
     private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
 
-    public static void init(){
-        for (ScheduledTask task : ScheduledTask.REGISTRY.values()){
+    public static void init() {
+        for (ScheduledTask task : ScheduledTask.REGISTRY.values()) {
             scheduler.scheduleAtFixedRate(
-                    task::execute,
+                    () -> {
+                        try {
+                            task.execute();
+                        } catch (Exception e) {
+                            Bot.LOGGER.error("Error executing scheduled task: {}", task.getName(), e);
+                            LoggingUtil.error(e);
+                        }
+                    },
                     0,
                     task.getInterval(),
                     task.getTimeUnit()
@@ -21,6 +29,7 @@ public class TaskRegistry {
             Bot.LOGGER.info("Registered Task: {}", task.getName());
         }
     }
+
 
     public static void shutdown(){
         scheduler.shutdown();
