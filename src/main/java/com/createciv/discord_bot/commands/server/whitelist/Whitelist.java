a60fe5ff5d29774cf8e35b31181a4aa12ee78542
EpicVon2468 <thomaschad0909@gmail.com>
2025-12-06 14:00:53 +1100

    quickly

--- src/main/java/com/createciv/discord_bot/commands/server/whitelist/Whitelist.java
+++ src/main/java/com/createciv/discord_bot/commands/server/whitelist/Whitelist.java
@@ -65,8 +65,7 @@ public class Whitelist extends SlashCommand {
 			.build();
 
 		Modal modal = Modal.create("whitelist", "Whitelist")
-			.addComponents(Label.of("Username", username))
-			.addComponents(Label.of("Referral", referral))
+			.addComponents(Label.of("Subject", username), Label.of("Body", referral))
 			.build();
 
 		interactionEvent.replyModal(modal).queue();
