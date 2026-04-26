8c293c869118c4aadd1432a34bd1c7dfd1e27694
AGannon22 <gannonalyssa@gmail.com>
2026-04-25 22:36:58 -0400

    added builder method to TicketEntry.java

--- src/main/java/com/createciv/discord_bot/util/database/types/TicketEntry.java
+++ src/main/java/com/createciv/discord_bot/util/database/types/TicketEntry.java
@@ -5,6 +5,7 @@ import com.createciv.discord_bot.util.database.TableEntry;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
+import java.util.UUID;
 
 public class TicketEntry extends TableEntry<TicketEntry> {
 
@@ -13,6 +14,7 @@ public class TicketEntry extends TableEntry<TicketEntry> {
 	public String threadChannelID;
 	public String embedMessageID;
 	public Timestamp lastActivity;
+	public Boolean active;
 
 	public TicketEntry(ResultSet resultSet) throws SQLException {
 		id = resultSet.getInt("id");
@@ -20,5 +22,55 @@ public class TicketEntry extends TableEntry<TicketEntry> {
 		threadChannelID = resultSet.getString("threadChannelID");
 		embedMessageID = resultSet.getString("embedMessageID");
 		lastActivity = resultSet.getTimestamp("lastActivity");
+		active = resultSet.getBoolean("active");
 	}
+	public TicketEntry(Builder builder){
+		embedMessageID = builder.embedMessageID;
+		threadChannelID= builder.threadChannelID;
+		authorID = builder.authorID;
+		lastActivity = builder.lastActivity;
+		active = builder.active;
+		id = builder.id;
+	}
+
+
+	public static class Builder {
+		private Boolean active;
+		private String authorID;
+		private Integer id;
+		private String threadChannelID;
+		private String embedMessageID;
+		private Timestamp lastActivity;
+
+		public Builder active(Boolean active) {
+			this.active = active;
+			return this;
+		}
+
+		public Builder lastActivity(Timestamp lastActivity) {
+			this.lastActivity = lastActivity;
+			return this;
+		}
+
+		public Builder threadChannelID(String threadChannelID) {
+			this.threadChannelID = threadChannelID;
+			return this;
+		}
+
+		public Builder embedMessageID(String embedMessageID) {
+			this.embedMessageID = embedMessageID;
+			return this;
+		}
+		public Builder authorID(String authorID) {
+			this.authorID = authorID;
+			return this;
+		}
+		public TicketEntry build(){
+			if (authorID == null) throw new IllegalStateException("authorID is null");
+			if (threadChannelID == null || embedMessageID == null) throw new IllegalStateException("either embed msg id or thread channel id is null");
+			return new TicketEntry(this);
+		}
+	}
+
+
 }
